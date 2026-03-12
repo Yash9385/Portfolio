0a579f29d2891fa145305256c67ff7a892c103f0
Yash9385 <yashsingh0120@gmail.com>
2026-03-12 10:51:42 +0530

    adding form button

--- client/main.js
+++ client/main.js
@@ -17,42 +17,6 @@ document.addEventListener("DOMContentLoaded", function () {
         showCursor: false
     });
 
-    /* ================= CONTACT FORM ================= */
-    const form = document.getElementById("contact-form");
-
-    form.addEventListener("submit", async (e) => {
-        e.preventDefault();
-
-        const data = {
-            name: document.getElementById("name").value,
-            email: document.getElementById("email").value,
-            subject: document.getElementById("subject").value,
-            message: document.getElementById("message").value,
-        };
-
-        try {
-            const res = await fetch("https://portfolio-1oiq.onrender.com/send-email", {
-                method: "POST",
-                headers: { 
-                    "Content-Type": "application/json"
-                },
-                body: JSON.stringify(data),
-            });
-
-            const result = await res.json();
-
-            if (result.success) {
-                alert("Message Sent Successfully ✅");
-                form.reset();
-            } else {
-                alert("Server error ❌");
-            }
-        } catch (err) {
-            alert("Connection error ❌");
-            console.error(err);
-        }
-    });
-
 });
 
 /* ================= SERVICE TOGGLE ================= */
